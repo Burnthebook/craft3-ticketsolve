55287ad9d7299c6b716587931af649bba7f2e975
dimitar <dimitar@burnthebook.co.uk>
2020-01-03 14:33:23 +0000

    expose show elements to Twig globally

--- src/twig/CraftVariableBehaviour.php
+++ src/twig/CraftVariableBehaviour.php
@@ -2,7 +2,9 @@
 namespace devkokov\ticketsolve\twig;
 
 use Craft;
+use devkokov\ticketsolve\elements\db\ShowQuery;
 use devkokov\ticketsolve\elements\db\VenueQuery;
+use devkokov\ticketsolve\elements\Show;
 use devkokov\ticketsolve\elements\Venue;
 use yii\base\Behavior;
 
@@ -11,6 +13,15 @@ class CraftVariableBehavior extends Behavior
     public function venues($criteria = null): VenueQuery
     {
         $query = Venue::find();
+        if ($criteria) {
+            Craft::configure($query, $criteria);
+        }
+        return $query;
+    }
+
+    public function shows($criteria = null): ShowQuery
+    {
+        $query = Show::find();
         if ($criteria) {
             Craft::configure($query, $criteria);
         }
